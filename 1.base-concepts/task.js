9dab6de887c179228cc8949b9014aa3ea54c3377
Az-of-Tul <a.tulaev@swissin.ru>
2023-07-05 11:55:46 +0300

    my first homework commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,17 @@
 "use strict"
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = (b ** 2 - 4 * a * c);
+  if (d < 0) {
+    return arr;
+  } else if (d === 0) {
+    arr.push(-b / (2 * a));
+    return arr;
+  }
+  arr.push((-b + Math.sqrt(d) ) / (2 * a));
+  arr.push((-b - Math.sqrt(d) ) / (2 * a));
   return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+solveEquation(4, 2, 8);
