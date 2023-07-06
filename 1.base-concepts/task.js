31d9c12470593de03a7d0d9713838f37af114cab
Az-of-Tul <a.tulaev@swissin.ru>
2023-07-06 12:03:18 +0300

    added homework 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,26 +2,31 @@
 
 function solveEquation(a, b, c) {
   let arr = [];
-  let d = (b ** 2 - 4 * a * c);
-  if (d < 0) {
-    return arr;
-  } else if (d === 0) {
+  let discriminant = b ** 2 - 4 * a * c;
+
+  if (discriminant === 0) {
     arr.push(-b / (2 * a));
-    return arr;
+  } else if (discriminant > 0) {
+    arr.push((-b + Math.sqrt(discriminant)) / (2 * a), (-b - Math.sqrt(discriminant)) / (2 * a));
   }
-  arr.push((-b + Math.sqrt(d) ) / (2 * a));
-  arr.push((-b - Math.sqrt(d) ) / (2 * a));
+
   return arr;
 }
 
-solveEquation(4, 2, 8);
+solveEquation(1, 5, 4);
+solveEquation(1, 2, 1);
+solveEquation(1, 2, 10)
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let P = percent / 100 / 12;
-  let n = countMonths;
+  let monthPercent = percent / 100 / 12;
   let S = amount - contribution;
-  let monthlyFee = S * (P + (P / (((1 + P)**n) - 1)))
-  let total = monthlyFee*countMonths;
-  return parseFloat(total.toFixed(2));
+  let monthPayment = S * (monthPercent + (monthPercent / ((Math.pow(1 + monthPercent, countMonths)) - 1)));
+  let mustPay = parseFloat((monthPayment * countMonths).toFixed(2));
+
+  return mustPay;
 }
-calculateTotalMortgage(10, 0, 50000, 12)
+calculateTotalMortgage(10, 0, 50000, 12);
+calculateTotalMortgage(10, 1000, 50000, 12);
+calculateTotalMortgage(10, 20000, 48);
+calculateTotalMortgage(10, 0, 10000, 36);
+calculateTotalMortgage(15, 0, 10000, 36);
