c0d61b0a0f7851219f1705af734d4cb0a5d6e12a
Az-of-Tul <a.tulaev@swissin.ru>
2023-07-06 03:05:59 +0300

    Решение задачи 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -15,3 +15,13 @@ function solveEquation(a, b, c) {
 }
 
 solveEquation(4, 2, 8);
+
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  let P = percent / 100 / 12;
+  let n = countMonths;
+  let S = amount - contribution;
+  let monthlyFee = S * (P + (P / (((1 + P)**n) - 1)))
+  let total = monthlyFee*countMonths;
+  return parseFloat(total.toFixed(2));
+}
+calculateTotalMortgage(10, 0, 50000, 12)
